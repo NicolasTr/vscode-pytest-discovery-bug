8a0992163207a8605e7914edc79acf52cb9ee619
Nicolas Tresegnie <nicolas.tresegnie@gmail.com>
2024-02-10 20:04:54 +0000

    Add files

---
+++ a/b/api/test_a_b.py
@@ -0,0 +1,6 @@
+def test_discovery_working():
+    """
+    Ensures we have at least one test per API.
+    This is necessary to see all APIs properly on VSCode after autodiscovery.
+    """
+    assert True
---
+++ e/f/api/test_e_f.py
@@ -0,0 +1,3 @@
+
+
+raise Exception('Intentionally fail autodiscovery')
